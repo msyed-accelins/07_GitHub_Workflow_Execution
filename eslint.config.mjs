1b5e55928a0b850ba8ce46b69961152f714836e8
msyed-accelins <mohammad.syed@accelins.com>
2025-09-11 15:46:30 +0100

    Updated eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("plugin:react/recommended", "plugin:react/jsx-runtime"),
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        semi: [2, "always"],
+    },
+}]);
